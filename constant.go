883ea97513231c393feda2e24fc3579c603d5880
swxctx <swxctxone@yeah.net>
2018-10-10 18:32:31 +0800

    增加常量定义

--- constant.go
+++ constant.go
@@ -6,3 +6,31 @@ const (
 	// SmallKing 小王
 	SmallKing string = "e2"
 )
+
+const (
+	// KingBombCount 王炸权重
+	KingBombCount = 10
+	// BigKingCount 大王权重
+	BigKingCount = 6
+	// SmallKingCount 小王权重
+	SmallKingCount = 4
+	// OneBoardsCount 单张权重
+	OneBoardsCount = 1
+	// TwoBoardsCount 单对权重
+	TwoBoardsCount = 2
+	// ThreeBoardsCount 三带一权重
+	ThreeBoardsCount = 3
+	// FourBoardsCount 炸弹权重
+	FourBoardsCount = 4
+)
+
+const (
+	// OneBoards
+	OneBoards = 1
+	// TwoBoards
+	TwoBoards = 2
+	// ThreeBoards
+	ThreeBoards = 3
+	// FourBoards
+	FourBoards = 4
+)
